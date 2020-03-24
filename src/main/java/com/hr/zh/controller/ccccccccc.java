1824d6417f9c1d4066a9b97e85048285ec81ed04
656771561 <656771561@163.com>
2020-03-24 15:32:50 +0800

    加了分支

---
+++ src/main/java/com/hr/zh/controller/ccccccccc.java
@@ -0,0 +1,10 @@
+package com.hr.zh.controller;
+
+import com.hr.ty.mapper.sys_rightMapper;
+
+public class ccccccccc {
+public static void main(String[] args) {
+	System.out.println("牛逼u是    方式发噶沙发沙发");
+}
+
+}
